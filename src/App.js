2feac569b4d049a87961e7f5a2c97e916f2d174a
Maarten86M <maartenmoelands@gmail.com>
2021-05-20 14:01:55 +0200

    opdracht gemaakt met een kleine beetje hulp van de uitwerking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div>
-      Schrijf hier jouw functie-component!
-    </div>
+    const [lightClick, setLightClick] = React.useState();
+
+    function handleClick(){
+        setLightClick(!lightClick);
+    }
+
+    return (
+        <button
+            type="button"
+            onClick={handleClick}
+        >
+            {lightClick ? "aan" : "uit"}
+        </button>
   );
 }
 
